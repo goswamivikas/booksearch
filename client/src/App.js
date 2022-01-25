fdf80454b9cb48843bf1a9b6cfa1cbb1b5ef82dc
goswamivikas <vikas.mca17.du@gmail.com>
2022-01-25 16:55:52 +0530

    added search bar and api call on Home screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from "@emotion/react";
+import Home from "screens/Home";
 
 function App() {
-  return <div className="App">Booksearch App</div>;
+  return (
+    <div
+      css={{
+        border: "1px dashed grey",
+        minHeight: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "30vh",
+      }}
+    >
+      <Home></Home>
+    </div>
+  );
 }
 
 export default App;
